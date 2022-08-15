598cbb7704558a93fa9cad0d84c2d88ff0b882f9
saingchildren <80457007+saingchildren@users.noreply.github.com>
2022-08-15 21:23:04 +0800

    2022/8/15 have problem

--- client/src/components/CreateForm/CreateForm.js
+++ client/src/components/CreateForm/CreateForm.js
@@ -3,16 +3,27 @@ import UserName from "./UserName"
 import { Form, Container, Row, Col, Button } from "react-bootstrap"
 import Context from "../../context/Context"
 import { useContextSelector } from "use-context-selector"
+import { Link } from "react-router-dom"
 
 export default function CreateForm () {
 
 	const username = useContextSelector(Context, item => item.username)
 	const socket = useContextSelector(Context, item => item.socket)
 
-	const createUser = () => {
-		if (!username) return alert("plz input username!")
+	const createUser = (e) => {
+
+		if (!username) {
+			e.preventDefault()
+			alert("plz input username!")
+		}
+
 		if (username) {
-			socket.emit("create", username)
+			socket.emit("create", username, (error) => {
+				if (error) {
+					e.preventDefault()
+					alert(error)
+				}
+			})
 		}
 	}
 
@@ -26,7 +37,11 @@ export default function CreateForm () {
 					<Col xs="8" lg="10"><UserName /></Col>
 				</Row>
 				<Row className="mt-3 text-center">
-					<Col><Button onClick={createUser} variant="secondary" size="lg">Create</Button></Col>
+					<Col>
+						<Link onClick={e => createUser(e)} to={`/home?username=${username}`}>
+							<Button variant="secondary" size="lg">Create</Button>
+						</Link>
+					</Col>
 				</Row>
 			</Container>
 		</>
