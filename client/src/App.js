358887eaabe69e0fc10cc28fffdb6e09ae53c206
saingchildren <80457007+saingchildren@users.noreply.github.com>
2022-08-18 05:46:18 +0800

    2022/8/18 react is so hard...

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from "react"
-import Router from "./router/Router"
-import Context from "./context/Context"
-import io from "socket.io-client"
+import React, { useState, useEffect } from "react";
+import Router from "./router/Router";
+import Context from "./context/Context";
+import io from "socket.io-client";
 
-export default function App () {
-	
+export default function App() {
+    document.body.style.backgroundColor = "#1E1E1E";
 
-	document.body.style.backgroundColor = "#1E1E1E"
-	
+    const [socket, setSocket] = useState("");
 
-	const [ socket, setSocket ] = useState("")
-	
-	useEffect(() => {
-		setSocket(io("http://localhost:3300"))
-	}, [])
+    useEffect(() => {
+        setSocket(io("http://localhost:3300"));
+    }, []);
 
-	const [ username, setUserName ] = useState("")
-	const [ room, setRoom ] = useState("")
+    const [username, setUserName] = useState("");
+    const [room, setRoom] = useState("");
 
-	return (
-		<Context.Provider value={{
-			username: username,
-			setUserName: setUserName,
-			room: room,
-			setRoom: setRoom,
-			socket: socket
-		}}>
-			<Router />
-		</Context.Provider>
-	)
+    return (
+        <Context.Provider
+            value={{
+                username: username,
+                setUserName: setUserName,
+                room: room,
+                setRoom: setRoom,
+                socket: socket,
+            }}
+        >
+            <Router />
+        </Context.Provider>
+    );
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,15 +5,27 @@ import ShowUsers from "./ShowUsers";
 
 export default function Home() {
     const socket = useContextSelector(Context, (item) => item.socket);
-    const [allusers, setAllUsers] = useState(null);
+    const [allusers, setAllusers] = useState("");
 
-    socket.on("send_users", (users) => {
-        setAllUsers(users);
-        console.log(`allusers: ${allusers}`);
-        console.log(
-            `get users: ${users.map((user) => user.id + ", " + user.username)}`
-        );
-    });
+    useEffect(() => {
+        socket.on("send_users", (users) => {
+            console.log(users);
+        });
 
-    return <>{allusers[0].username}</>;
+        return () => {
+            socket.off("send_users");
+        };
+    }, []);
+
+    return (
+        <>
+            <button
+                onClick={() => {
+                    console.log(allusers);
+                }}
+            >
+                show
+            </button>
+        </>
+    );
 }
