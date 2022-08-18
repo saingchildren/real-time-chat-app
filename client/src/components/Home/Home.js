ef6a44de2b5412631f1b6c6de73495fea5d5e677
saingchildren <80457007+saingchildren@users.noreply.github.com>
2022-08-18 21:17:31 +0800

    2022/8/18 connect & disconnect done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,28 +4,10 @@ import Context from "../../context/Context";
 import ShowUsers from "./ShowUsers";
 
 export default function Home() {
-    const socket = useContextSelector(Context, (item) => item.socket);
-    const [allusers, setAllusers] = useState("");
-
-    useEffect(() => {
-        socket.on("send_users", (users) => {
-            console.log(users);
-        });
-
-        return () => {
-            socket.off("send_users");
-        };
-    }, []);
 
     return (
         <>
-            <button
-                onClick={() => {
-                    console.log(allusers);
-                }}
-            >
-                show
-            </button>
-        </>
+			<ShowUsers />
+		</>
     );
 }
--- client/src/components/Home/ShowUsers.js
+++ client/src/components/Home/ShowUsers.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import Context from "../../context/Context";
+import { useContextSelector } from "use-context-selector";
 
-export default function ShowUsers ({ allusers }) {
+export default function ShowUsers() {
+    const socket = useContextSelector(Context, (item) => item.socket);
+    const [alluser, setAlluser] = useState([]);
 
-	console.log(`showuser ${allusers}`)
+    const get_data = () => {
+        socket.emit("load_users");
+        socket.on("send_users", (users) => {
+            setAlluser(users);
+        });
+    };
 
-	return (
-		<div>
-			{allusers.map((user) => `${user.id} ${user.username}`)}
-		</div>
-	)
+    useEffect(() => {
+        get_data();
+        console.log(alluser);
+    }, []);
+
+    return <div style={{ background: "white" }}>{alluser.length}</div>;
 }
